7b2d96117aca9c34c2ee2bf81cf63b73fa4e64cf
Christopher Galias <chris.galias@gmail.com>
2021-10-21 11:03:17 +0200

    Update tests with TrajectoryBatch

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import pytest
 import torch
-import numpy as np
-from gym.wrappers import LazyFrames
 
 from actor_critic import ActorCritic
-from buffer import TrajectoryBuffer
+from buffer import TrajectoryBatch, TrajectoryBuffer
 
 
 class TestActorCritic:
@@ -21,23 +19,22 @@ class TestActorCritic:
 class TestTrajectoryBuffer:
     @pytest.fixture
     def example_tb(self):
-        return TrajectoryBuffer(10, (3, 3, 3), (2,), 0.1, 0.5)
+        return TrajectoryBuffer(10, (3, 3, 3), (2,), 0.1, 0.5, False)
 
     @pytest.fixture
     def example_args(self):
-        obs = LazyFrames(np.arange(stop=3 * 3 * 3).reshape((1, 3, 3, 3)))
+        obs = torch.arange(end=3 * 3 * 3).view(1, 3, 3, 3)
         act = torch.Tensor([1., 2.])
-        act_log_prob = 0.2
+        act_log_prob = torch.Tensor([0.2])
         val = torch.Tensor([1.])
-        rew = 0.1
-        return obs, act, act_log_prob, val, rew
+        rew = torch.Tensor([0.1])
+        return TrajectoryBatch(obs=obs, act=act, act_log_prob=act_log_prob, val=val, rew=rew)
 
     def test_store(self, example_tb, example_args):
-        example_tb.store(*example_args)
+        example_tb.store(example_args)
         assert example_tb.current_length == 1
 
     def test_retrieve_when_not_full(self, example_tb, example_args):
-        example_tb.store(*example_args)
+        example_tb.store(example_args)
         with pytest.raises(Exception):
             _ = example_tb.retrieve_and_clear()
-
